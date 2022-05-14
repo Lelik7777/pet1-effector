68141768b3142abfe2b44f4fe818af6b4aa74423
Lelik Jan <21alex7777@gmail.com>
2022-05-14 13:49:33 +0300

    add local storage for Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,30 @@
 import {createComponent, useStore} from "effector-react";
-import {createEvent, createStore} from "effector";
+import {createEvent, createStore, sample} from "effector";
+import {useEffect} from "react";
 
 const increment = createEvent();
 const increment2 = createEvent();
 const reset = createEvent();
-export const $counter = createStore(0)
-    .on(increment, state => state + 1)
-    .reset(reset);
+let value = JSON.parse(localStorage.getItem('counter'));
+console.log(typeof value)
+export const $counter = createStore(value)
+    .on(increment, state => state + 1).on(reset, (state, d) => d = 0);
 export const $counter2 = createStore(0)
     .on(increment2, state => state + 1);
-
+sample({
+    clock: $counter,
+    source: $counter,
+    fn: (counter) => localStorage.setItem('counter', JSON.stringify(counter)),
+    target: $counter
+})
 
 export const Counter = () => {
     const state = useStore($counter);
+
+
     return (<div>
         Counter: {state}
-        <button onClick={increment} disabled={state >= 10}>+</button>
+        <button onClick={increment}>+</button>
         <button onClick={reset}>reset</button>
     </div>)
 }
@@ -23,6 +32,6 @@ export const Counter = () => {
 export const Counter2 = createComponent($counter2, (props, state) => (
     <div>
         Counter2: {state}
-        <button onClick={increment2} disabled={state>4}>+</button>
+        <button onClick={increment2} disabled={state > 4}>+</button>
     </div>
 ))
--- src/MyInput.js
+++ src/MyInput.js
@@ -1,31 +1,35 @@
-import {$counter,$counter2} from "./Counter";
+import {$counter, $counter2} from "./Counter";
 import {useStore} from "effector-react";
 import {createEvent, createStore, sample} from "effector";
 
 const setCounter = createEvent();
-const setCounter2=createEvent();
+const setCounter2 = createEvent();
 const resetInput = createEvent();
 
 const onChange = createEvent();
-const onChange2=createEvent();
+const onChange2 = createEvent();
 const $input = createStore(0)
-    .on(onChange, (state,num) => num)
+    .on(onChange, (state, num) => num)
     .reset(resetInput);
-const $input2=createStore(0)
-    .on(onChange2,(state,num)=>num);
+const $input2 = createStore(0)
+    .on(onChange2, (state, num) => num);
+
 sample({
     clock: setCounter,
     source: $input,
     target: $counter
 });
+
 sample({
-    clock:$input2,
-    source:$input2,
-    target:$counter2
+    clock: $input2,
+    source: $input2,
+    target: $counter2
 })
+
+
 export const MyInput = () => {
     const state = useStore($input);
-    const state2=useStore($input2)
+    const state2 = useStore($input2)
     let onClick = () => {
         setCounter();
         resetInput();
@@ -35,7 +39,7 @@ export const MyInput = () => {
             <input type='number' value={state} onChange={(e) => onChange(e.target.value)}/>
             <button onClick={onClick}>set counter</button>
             <br/>
-            <input type="number" value={state2} onChange={(e)=>onChange2(e.target.value)}/>
+            <input type="number" value={state2} onChange={(e) => onChange2(e.target.value)}/>
 
         </div>
     )
