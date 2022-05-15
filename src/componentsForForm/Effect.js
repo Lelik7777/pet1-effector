87075174bbb7905ba5ce1b9f9a2ee9673fa5129b
Lelik Jan <21alex7777@gmail.com>
2022-05-15 17:34:00 +0300

    add preloader

--- src/componentsForForm/Effect.js
+++ src/componentsForForm/Effect.js
@@ -1,8 +1,9 @@
 import {createEffect, createEvent, createStore} from "effector";
 import {useStore} from "effector-react";
 import axios from "axios";
+import {Preloader} from "./Preloader";
 
-
+//search film by name
 const sendReqFx = createEffect(async ({title}) => {
 
     const url = `http://www.omdbapi.com/?s=${title}&apikey=e786857c`
@@ -21,12 +22,17 @@ const $input = createStore('').on(onChange, (state, string) => string).reset(cle
 export const Effect = () => {
     const value = useStore($input);
     const data = useStore($store);
+    const loading = useStore(sendReqFx.pending);
     let onChangeInput = (e) => onChange(e.target.value);
-    return (<div style={{marginTop:'20px'}}>
+    return (<div style={{marginTop: '20px'}}>
         <input type="text" placeholder='search film' value={value} onChange={onChangeInput}/>
         <button onClick={() => sendReqFx({title: value})}>find</button>
         <button onClick={clearStore}>reset</button>
-        <div> array length{data.length}</div>
+        <div>
+            array length{data.length}
+            {loading&&<Preloader/>}
+        </div>
+
         <div>{data.map(x => <div style={{marginBottom: '10px', width: '40%'}}>{JSON.stringify(x)}</div>)}</div>
     </div>)
 }
